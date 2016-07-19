5f7ff80a4d5f9db84a79f21497ce56ba61b8f233
Sahan Fernando <sahan.h.fernando@gmail.com>
2016-07-19 20:25:09 +1000

    Intermediate Progress on porting visual.c to Allegro5

---
+++ visual.c
@@ -0,0 +1,338 @@
+#include<allegro5/allegro.h>
+#include<allegro5/allegro_primitives.h>
+#include<stdio.h>
+#include<stdint.h>
+#include<errno.h>
+#include<semaphore.h>
+#include<pthread.h>
+#include<limits.h>
+#include<unistd.h>
+#include<complex.h>
+#include<math.h>
+#include<assert.h>
+#include<string.h>
+
+#define ERROR(x) {fprintf(stderr, "Error in %s(): " x "\n", __func__);exit(EXIT_FAILURE);}
+#define PARAMETER_ERROR() ERROR("Invalid Parameters provided");
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define CLAMP(min,val,max) MAX((min),MIN((max),(val)))
+
+//44100/20 = 2205
+//#define FRAGLENGTH 2205
+
+//framerate doesn't need to be exact, but 
+//FRAGLENGTH needs to be power of two
+#define FRAGLENGTH 2048
+//channel: 0=left, 1=right
+int16_t buf[FRAGLENGTH][2][2] = { 0 };// [n][channel][buffer]
+int n=0;
+sem_t lock[2];
+pthread_t renderThread;
+
+int quit=0;
+
+int fgetw(FILE * fp){
+    int a=fgetc(fp);
+    if(a==EOF){
+        return EOF;
+    }
+    int b=fgetc(fp);
+    if(b==EOF){
+        return EOF;
+    }
+    uint16_t retval=((b<<8)+a);
+    return retval;
+}
+
+void fputw(int16_t val, FILE * fp){
+    fputc(val&0xffu,fp);
+    fputc(val>>8,fp);
+}
+
+int roundToNextPow2(int in){
+    in--;
+    in|=in>>1;
+    in|=in>>2;
+    in|=in>>4;
+    in|=in>>8;
+    in|=in>>16;
+    return in+1;
+}
+
+void compute_dft(double complex in[], double complex out[], int n){
+    for(int i = 0; i < n; ++i){
+        double sumRe=0.0;
+        for(int j = 0; j < n; ++j){
+            double angle = 2 * M_PI * i * j / n;
+            sumRe+=cos(angle)*in[j];
+        }
+        out[i]+=sumRe+0.0I;
+    }
+}
+
+void swapDC(double complex * a, double complex * b){
+    double complex temp = *a;
+    *a=*b;
+    *b=temp;
+}
+
+unsigned int reverseByteOrder(unsigned int input, unsigned int numBits){
+    unsigned int retVal=0;
+    if(input >= (1U << numBits)){
+        PARAMETER_ERROR();
+    }
+    for(unsigned int i = 0; i < numBits; ++i){
+        retVal<<=1;
+        retVal |= input&1;
+        input>>=1;
+    }
+    return retVal;
+}
+
+void compute_fft(double complex data[], unsigned int n){
+    //check n is power of two
+    int found=0;
+    unsigned int logN=1;
+    for(unsigned int i = 2; i!=0; i<<=1, logN++){
+        if(i==n){
+            found=1;
+            break;
+        }
+    }
+    if(!found){
+        fprintf(stderr, "Error: call to %s(..., n=%d)\n", __func__, n);
+        exit(EXIT_FAILURE);
+    }
+    //sort by reverse byte order
+    //In this case, we are doing this in place
+    for(unsigned int i = 0; i < n; ++i){
+        unsigned int rev = reverseByteOrder(i,logN);
+        if(i<rev){
+            swapDC(&data[i],&data[rev]);
+        }
+    }
+    //Perform Danielson Lanczos fft
+    unsigned int mmax = 1;
+    while(n > mmax){
+        unsigned int istep = 2 * mmax;
+        double theta = -M_PI/mmax;
+        double temp = sin(theta/2.0);
+        double complex wp = -2.0*temp*temp + sin(theta)*I;
+        double complex w = 1.0 + 0.0I;
+        for(unsigned int m=0; m<mmax; ++m){
+            for(unsigned int i=m; i<n; i+=istep){
+                unsigned int j = i+mmax;
+                double complex temp = w * data[j];
+                data[j]=data[i]-temp;
+                data[i]=data[i]+temp;
+            }
+            w = w * wp + w;
+        }
+        mmax = istep;
+    }
+}
+
+void drawVisualizer(ALLEGRO_DISPLAY * display, uint16_t width, uint16_t height, int readBuffer, double complex * leftArr[2], double complex * rightArr[2], int arrLength){
+    static unsigned int flip=0;
+    int pos = 0;
+    int16_t max[width];
+    int16_t min[width];
+    //al_draw_filled_rectangle(0, 0, width, height, al_map_rgb(99, 99, 99));
+    al_draw_filled_rectangle(0, 0, width, height, al_map_rgb(50, 50, 50));
+    for(uint16_t ix=0; ix<width; ++ix){
+        max[ix]=SHRT_MIN;
+        min[ix]=SHRT_MAX;
+        --pos;
+        do{
+            ++pos;
+            max[ix]=MAX(max[ix],buf[pos][0][readBuffer]);
+            max[ix]=MAX(max[ix],buf[pos][1][readBuffer]);
+            min[ix]=MIN(min[ix],buf[pos][0][readBuffer]);
+            min[ix]=MIN(min[ix],buf[pos][1][readBuffer]);
+            leftArr [flip&1][pos]=buf[pos][0][readBuffer];
+            rightArr[flip&1][pos]=buf[pos][1][readBuffer];
+        }while(pos<(ix*FRAGLENGTH)/(width-1));
+        //max[ix]=MIN(max[ix],0);
+        //min[ix]=MAX(min[ix],0);
+    }
+    compute_fft( leftArr[flip&1],arrLength);
+    compute_fft(rightArr[flip&1],arrLength);
+    int transformRects=128;
+    pos=0;
+    for(uint16_t i=0; i < transformRects; ++i){
+
+        uint16_t x0=(i*(width-1))/transformRects;
+        uint16_t x1=((i+1)*(width-1))/transformRects;
+
+        double rectAmpL=0.0f;
+        double rectAmpR=0.0f;
+        int numInChunk=0;
+
+        while(pos<(transformRects*i)/(transformRects)){
+            rectAmpL +=  leftArr[(flip&1)^0][pos]*0.8;
+            rectAmpR += rightArr[(flip&1)^0][pos]*0.8;
+            rectAmpL +=  leftArr[(flip&1)^1][pos]*0.2;
+            rectAmpR += rightArr[(flip&1)^1][pos]*0.2;
+            ++pos;
+            ++numInChunk;
+        }
+        rectAmpL/=(double)numInChunk;
+        rectAmpR/=(double)numInChunk;
+
+        uint16_t thisHeightl=CLAMP(0,sqrt(fabs(rectAmpL))/(sqrt(arrLength)/2),160);
+        uint16_t thisHeightr=CLAMP(0,sqrt(fabs(rectAmpR))/(sqrt(arrLength)/2),160);
+        al_draw_filled_rectangle(x0,1,x1,1+thisHeightl,               al_map_rgb(255, 0, 0));
+        al_draw_filled_rectangle(x0,height-1-thisHeightr,x1,height-1, al_map_rgb(255, 0, 0));
+        //al_draw_filled_rectangle(x0,height/2-thisHeightl,x1,height/2, al_map_rgb(255, 0, 0));
+        //al_draw_filled_rectangle(x0,height/2,x1,height/2+thisHeightr, al_map_rgb(255, 0, 0));
+    }
+    for(uint16_t ix=0; ix<width; ++ix){
+        int16_t minNormalized=((int)(min[ix])*height)/(2*SHRT_MAX);
+        int16_t maxNormalized=((int)(max[ix])*height)/(2*SHRT_MAX);
+        al_draw_line(1+ix,height/2+minNormalized,1+ix,height/2+maxNormalized,al_map_rgb(0x40,0x0ff,0xff),1);
+    }
+    flip++;
+}
+
+void * renderLoop(void * parameter){
+    unsigned int flip=0;
+    unsigned int fftArrLength=roundToNextPow2(FRAGLENGTH);
+    double complex * leftArr [2];
+    double complex * rightArr[2];
+    uint16_t width=640;
+    uint16_t height=320;
+    leftArr [0] = malloc(sizeof(double complex)*fftArrLength);
+    leftArr [1] = malloc(sizeof(double complex)*fftArrLength);
+    rightArr[0] = malloc(sizeof(double complex)*fftArrLength);
+    rightArr[1] = malloc(sizeof(double complex)*fftArrLength);
+    //ALLEGRO_DISPLAY * display = (ALLEGRO_DISPLAY*) parameter;
+    ALLEGRO_DISPLAY * display;
+    if(al_init()==0){
+        ERROR("Couldn't initialize Allegro");
+        exit(EXIT_FAILURE);
+    }
+
+    al_set_new_display_flags(ALLEGRO_RESIZABLE | ALLEGRO_NOFRAME);
+    display = al_create_display(width, height);
+    int readBuffer=1;
+
+    for(unsigned int i = 0; i < fftArrLength; ++i){
+        leftArr [1][i]=0.0+0.0I;
+        rightArr[1][i]=0.0+0.0I;
+    }
+
+    while(!quit){
+        for(unsigned int i = 0; i < fftArrLength; ++i){
+            leftArr [flip&1][i]=0.0+0.0I;
+            rightArr[flip&1][i]=0.0+0.0I;
+        }
+        ++flip;
+        sem_wait(&lock[readBuffer]);
+        drawVisualizer(display, width, height, readBuffer, leftArr, rightArr, fftArrLength);
+        al_flip_display();
+        sem_post(&lock[readBuffer]);
+        readBuffer=!readBuffer;
+    }
+    free( leftArr[0]);
+    free(rightArr[0]);
+    free( leftArr[1]);
+    free(rightArr[1]);
+    return NULL;
+}
+
+void getMonitorName(char * dest){
+    FILE * pactl = popen("pactl list short sinks", "r");
+    if(pactl==NULL){
+        ERROR("Could not open pactl");
+        exit(EXIT_FAILURE);
+    }
+    char dataBuf[2048];
+    char * monitors[32];
+    monitors[0]=dataBuf;
+    int nMonitors=1;
+    int n = 0;
+    char b;
+    while((b=fgetc(pactl))!=EOF){
+        dataBuf[n++]=b;
+    }
+    dataBuf[n]='\0';
+    for(int i = 0; i < n; ++i){
+        if(dataBuf[i]=='\n'&&i+1<n){
+            monitors[nMonitors++]=&dataBuf[i+1];
+            dataBuf[i]='\0';
+        }
+    }
+    for(int i = 0; i < nMonitors; ++i){
+        if(strstr(monitors[i],"RUNNING")!=NULL){
+            strtok(monitors[i]," \t");
+            char * name = strtok(NULL," \t");
+            strncpy(dest,name,256);
+        }
+    }
+    pclose(pactl);
+}
+
+void tests(){
+    assert(reverseByteOrder(2,4)==4);
+    assert(reverseByteOrder(4,5)==4);
+    assert(reverseByteOrder(255,8)==255);
+}
+
+int main(){
+    tests();
+    char cmdbuf[256] = { 0 };
+    char inputMonitorName[256] = { 0 };
+    getMonitorName(inputMonitorName);
+    if(!inputMonitorName[0]){
+        ERROR("Could not find running pulse sink");
+        return EXIT_FAILURE;
+    }
+    sprintf(cmdbuf, "pacat --raw --record --latency-msec=10 --format s16le -d %s.monitor", inputMonitorName);
+
+    FILE * inputDevice = popen(cmdbuf, "r");
+    if(inputDevice==NULL){
+        ERROR("Couldn't popen(\"pacat ...\")");
+        fprintf(stderr, "ERRNO: %d\n", errno);
+        return EXIT_FAILURE;
+    }
+    if(sem_init(&lock[0],0,1)==-1||sem_init(&lock[1],0,1)){
+        ERROR("Couldn't initialize semaphores");
+        fprintf(stderr, "ERRNO: %d\n", errno);
+        return EXIT_FAILURE;
+    }
+
+    //ALLEGRO_DISPLAY * display;
+    //if(al_init()==0){
+    //    ERROR("Couldn't initialize Allegro");
+    //    return EXIT_FAILURE;
+    //}
+
+    //al_set_new_display_flags(ALLEGRO_RESIZABLE);
+    //display = al_create_display(640, 320);
+
+    //if(pthread_create(&renderThread, NULL, renderLoop, display)!=0){
+    if(pthread_create(&renderThread, NULL, renderLoop, NULL)!=0){
+        ERROR("Spawn Render() thread");
+        fprintf(stderr, "ERRNO: %d\n", errno);
+        return EXIT_FAILURE;
+    }
+    int channel=0;
+    int writeBuffer=0;
+    sem_wait(&lock[writeBuffer]);
+    for(int b=fgetw(inputDevice); b!=EOF; b=fgetw(inputDevice)){
+        buf[channel?n++:n][channel][writeBuffer]=b;
+        channel=!channel;
+        if(n==FRAGLENGTH){
+            sem_post(&lock[writeBuffer]);
+            writeBuffer=!writeBuffer;
+            sem_wait(&lock[writeBuffer]);
+            n=0;
+        }
+    }
+    sem_post(&lock[writeBuffer]);
+    quit=1;
+    pclose(inputDevice);
+    pthread_cancel(renderThread);
+    return EXIT_SUCCESS;
+}
